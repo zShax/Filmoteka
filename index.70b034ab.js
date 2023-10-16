1e0782765746bf40085cd92a538b3ac58fe3f058
Razvan49 <128290757+Razvan49@users.noreply.github.com>
2023-10-16 21:37:49 +0000

    Deploying to gh-pages from @ Razvan49/parcel-project-template@14faadac2b71db76c9afa758041d4be7c8572026 🚀

---
+++ index.70b034ab.js
@@ -0,0 +1,2 @@
+!function(){console.log("PRIMUL JS");console.log("AL DOILEA JS")}();
+//# sourceMappingURL=index.70b034ab.js.map
---
+++ index.ab79466b.js
@@ -0,0 +1,2 @@
+console.log("PRIMUL JS");console.log("AL DOILEA JS");
+//# sourceMappingURL=index.ab79466b.js.map
